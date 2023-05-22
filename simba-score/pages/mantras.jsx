308858da5c96b5d67b8f517f62f2f307c82b39ad
minkman <lomick2090@gmail.com>
2023-05-22 16:09:19 -0500

    add buttons

--- simba-score/pages/mantras.jsx
+++ simba-score/pages/mantras.jsx
@@ -1,13 +1,28 @@
 import Layout from "../components/Layout"
+import styles from "../styles/mantras.module.css"
 import { useMantraContext } from "../context/mantrasContext"
 
 export default function Mantras() {
     const mantras = useMantraContext()
     
+    let rawMantraTags = []
+    mantras.map(mantra => {
+        mantra.tags.map(tag => {
+            rawMantraTags.push(tag)
+        })
+    })
+    let mantraTags = [... new Set(rawMantraTags)]
+
+    const mantraButtons = mantraTags.map(tag => {
+        return <button key={tag} className={styles.mantrabuttons}>{tag}</button>
+    })
 
     return (
         <Layout>
+            <div className={styles.page}>
+                {mantraButtons}
 
+            </div>
         </Layout>
     )
 }
